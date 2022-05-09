0d347b77fc8cdddcaa5bd4bcf67c92190a6da4c0
HansWeda <hans.weda@vng.nl>
2022-05-09 16:10:40 +0200

    Update of the tests

---
+++ tests/testthat/test_vertaal_postcode_naar_regio_op_peiljaar.R
@@ -0,0 +1,31 @@
+library(grenswijzigen)
+require(cbsodataR)
+require(dplyr)
+
+
+test_that("vertaal data op postcode niveau naar wijkniveau", {
+
+  # laad de bevolkingsgrootte per 4-cijferige postcode
+  df.postcode <- cbs_get_data(
+    id = "83502NED",
+    Geslacht = "T001038",
+    Leeftijd = "10000",
+    Postcode = has_substring("PC"),
+    Perioden = "2020JJ00",
+    select = c("Geslacht", "Leeftijd", "Postcode", "Perioden", "Bevolking_1")
+  ) %>% mutate(
+    Postcode = gsub("\\D", "", Postcode)
+  ) %>% select("Postcode", "Bevolking_1")
+
+  # zet om naar wijk
+  df.vertaald = vertaal_postcode_naar_regio_op_peiljaar(
+    df.postcode,
+    oorspronkelijk_jaar = 2020,
+    peiljaar = 2021,
+    type_kolommen = "aantal",
+    regionaalniveau = "wijk"
+  )
+
+  # Hieronder staat de omgezette postcode data
+  expect_snapshot(head(df.vertaald))
+})
